cd608b4eb56b6b593ab54ae485455783e2d493b0
Henrique Miguel Deschamps Dahmer <henriquedahmer1234@gmail.com>
2026-06-10 08:44:12 -0300

    Delete Copa-do-Mundo directory

--- Copa-do-Mundo/atividade/atividade/src/main/java/copa_do_mundo/atividade/Repository/ConvocacaoRepository.java
+++
@@ -1,7 +0,0 @@
-package copa_do_mundo.atividade.Repository;
-
-import copa_do_mundo.atividade.Model.Convocacao;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ConvocacaoRepository extends JpaRepository<Convocacao, Long> {
-}
